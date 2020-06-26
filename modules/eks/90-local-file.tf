bc5534dd7c21c9b69434b9eb363e4df838e6f003
jamje <gelius7@gmail.com>
2020-06-26 18:15:13 +0900

    output file path fix

--- modules/eks/90-local-file.tf
+++ modules/eks/90-local-file.tf
@@ -2,15 +2,15 @@
 
 resource "local_file" "aws_auth" {
   content  = data.template_file.aws_auth.rendered
-  filename = "${path.cwd}/.output/aws_auth.yaml"
+  filename = "${path.module}/.output/aws_auth.yaml"
 }
 
 resource "local_file" "kube_config" {
   content  = data.template_file.kube_config.rendered
-  filename = "${path.cwd}/.output/kube_config.yaml"
+  filename = "${path.module}/.output/kube_config.yaml"
 }
 
 resource "local_file" "kube_config_secret" {
   content  = data.template_file.kube_config_secret.rendered
-  filename = "${path.cwd}/.output/kube_config_secret.yaml"
+  filename = "${path.module}/.output/kube_config_secret.yaml"
 }
