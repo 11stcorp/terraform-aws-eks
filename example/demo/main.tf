92a2887e5f8f41b149a1b862087ca5e9bf4663f6
jamje <gelius7@gmail.com>
2020-07-05 19:12:58 +0900

    new module

---
+++ example/demo/main.tf
@@ -0,0 +1,37 @@
+# eks
+
+terraform {
+  backend "s3" {
+    region         = "ap-northeast-2"
+    bucket         = "terraform-11st-demo-state"
+    key            = "eks-demo.tfstate"
+  }
+  required_version = ">= 0.12"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source = "../../"
+
+  region = var.region
+  name   = var.name
+
+  kubernetes_version = var.kubernetes_version
+
+  vpc_id     = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids = data.terraform_remote_state.vpc.outputs.private_subnet_ids
+
+  allow_ip_address = var.allow_ip_address
+
+  workers = [
+    "arn:aws:iam::${local.account_id}:role/${var.name}-worker",
+    "arn:aws:iam::${local.account_id}:role/${var.name}-private",
+    "arn:aws:iam::${local.account_id}:role/${var.name}-public",
+  ]
+
+  map_roles = local.map_roles
+  map_users = local.map_users
+}
